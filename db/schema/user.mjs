948bff182b92b19be4de36d83b58a32e7a4e0e36
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2025-12-11 21:36:48 +0100

    Add verification fields

--- db/schema/user.mjs
+++ db/schema/user.mjs
@@ -1,7 +1,17 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { sql } from "drizzle-orm";
+import {
+  boolean,
+  integer,
+  pgTable,
+  timestamp,
+  varchar,
+} from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   email: varchar({ length: 255 }).notNull().unique(),
+  verified: boolean().default(sql`FALSE`),
+  verificationToken: varchar({ length: 255 }),
+  verificationTokenExpires: timestamp(),
   password: varchar({ length: 255 }).notNull(),
 });
