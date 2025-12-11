f1e763ae80e6c50ef4fcca84b46843448e5e0201
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2025-12-11 11:55:07 +0100

    Update app.mjs

--- app.mjs
+++ app.mjs
@@ -1,39 +1,30 @@
-import { dirname } from "node:path";
-import { fileURLToPath } from "node:url";
-
 import express from "express";
 import cookieParser from "cookie-parser";
 import createError from "http-errors";
 
 import logger from "./middlewares/pino-http.mjs";
 
-import { config } from "dotenv";
+import { authRouter } from "./routes/auth.mjs";
+import { errorHandler } from "./middlewares/errorHandler.mjs";
 
-import indexRouter from "./routes/index.mjs";
-import usersRouter from "./routes/users.mjs";
+import { config } from "dotenv";
 
 config();
 
 let app = express();
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
-// view engine setup
-app.set("views", __dirname + "/views");
-app.set("view engine", "hbs");
-
 app.use(logger);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(__dirname + "/public"));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use("/auth", authRouter);
 
 // catch 404 and forward to error handler
 app.use((_1, _2, next) => {
   next(createError(404));
 });
+
+app.use(errorHandler);
 
 export default app;
