ba36af4128a10060b656e050afba60a22e90a367
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2026-01-17 08:55:53 +0100

    Fix login dto

--- dtos/auth.dto.mjs
+++ dtos/auth.dto.mjs
@@ -1,28 +1,27 @@
 import z from "zod";
+import { eq } from "drizzle-orm";
+import bcrypt from "bcrypt";
 
-export const authDto = z
+import db from "../db/index.mjs";
+import { usersTable } from "../db/schema/user.mjs";
+
+export const loginDto = z
   .object({
     email: z.email(),
     password: z.string().min(8).max(30),
   })
   .superRefine(async (val, ctx) => {
-    const result = await userRepo.find({ username: val.username });
-    if (!result.success) {
-      ctx.addIssue({
-        code: "custom",
-        path: ["username"],
-        message: "DB Error",
-      });
+    const results = await db
+      .select()
+      .from(usersTable)
+      .where(eq(usersTable.email, val.email));
 
-      return;
-    }
-
-    const user = result.results[0];
+    const user = results[0];
 
     if (!user) {
       ctx.addIssue({
         code: "custom",
-        path: ["username"],
+        path: ["email"],
         message: "User not found",
       });
       return;
@@ -31,7 +30,7 @@ export const authDto = z
     if (!user?.emailverified) {
       ctx.addIssue({
         code: "custom",
-        path: ["username"],
+        path: ["email"],
         message: "Email not verified",
       });
 
